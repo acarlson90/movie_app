87b25148ae3a23dc8482a3b995cc3b5a38d2d025
Aaron Carlson <acarlson90@gmail.com>
2014-05-25 13:44:55 -0700

    Include Guard and Spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Aaron's Movie Database App" }
+
   describe "Home Page" do
 
   	# description is just a string, and it can be anything you want; RSpec doesn't care
@@ -14,7 +16,7 @@ describe "Static Pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Aaron's Movie Database App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -27,7 +29,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Aaron's Movie Database App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -40,7 +42,20 @@ describe "Static Pages" do
 
     it "should have the title 'About Me'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Aaron's Movie Database App | About Me")
+      expect(page).to have_title("#{base_title} | About Me")
+    end
+  end
+
+  describe "Contact Page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
